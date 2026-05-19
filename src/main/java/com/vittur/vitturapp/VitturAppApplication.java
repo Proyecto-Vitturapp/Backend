ef0969d689e4c0695d82f49fbbc084cceee2a3e6
nilcrepa-ops <nilcrepa@gmail.com>
2026-05-19 09:27:54 +0200

    First commit, nothing created

---
+++ src/main/java/com/vittur/vitturapp/VitturAppApplication.java
@@ -0,0 +1,13 @@
+package com.vittur.vitturapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitturAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitturAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vittur/vitturapp/VitturAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittur.vitturapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitturAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
